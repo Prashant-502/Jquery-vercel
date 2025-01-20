a4b7ddaf72f5895bbb68a53cfb0bf71d776f82cf
Prashant-0207 <prashantshigvanofficial@gmail.com>
2025-01-20 17:36:03 +0530

    slick carousel

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,15 @@
     var year = new Date().getFullYear();
     document.querySelector("#currentYear").innerHTML = year;
 })();
+var slideWrapper = $(".main-slider");
+slideWrapper.slick({
+    // fade:true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    autoplaySpeed: 1000,
+    lazyLoad: "progressive",
+    speed: 600,
+    arrows: false,
+    dots: true,
+    cssEase: "cubic-bezier(0.87, 0.03, 0.41, 0.9)"
+});
