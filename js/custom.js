c6220ea2bfbdefa718383d6028358c6387509bf3
Prashant-0207 <prashantshigvanofficial@gmail.com>
2025-01-21 17:16:51 +0530

    web worker js

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,17 @@
-// get current year
-(function () {
+document.addEventListener("DOMContentLoaded", function () {
     var year = new Date().getFullYear();
     document.querySelector("#currentYear").innerHTML = year;
-})();
-var slideWrapper = $(".main-slider");
-slideWrapper.slick({
-    // fade:true,
-    autoplay: false,
-    autoplaySpeed: 3000,
-    autoplaySpeed: 1000,
-    lazyLoad: "progressive",
-    speed: 600,
-    arrows: false,
-    dots: true,
-    cssEase: "cubic-bezier(0.87, 0.03, 0.41, 0.9)"
+    
+    var slideWrapper = $(".main-slider");
+    slideWrapper.slick({
+        // fade:true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        autoplaySpeed: 1000,
+        lazyLoad: "progressive",
+        speed: 600,
+        arrows: false,
+        dots: true,
+        cssEase: "cubic-bezier(0.87, 0.03, 0.41, 0.9)"
+    });
 });
